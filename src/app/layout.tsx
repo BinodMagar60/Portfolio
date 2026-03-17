44cf3ba9b008b6b0e145c719f417e3773408b1f5
BinodMagar60 <kauchabinod88@gmail.com>
2026-03-17 22:48:47 +0545

    feat: no speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"]
@@ -135,7 +134,6 @@ export default function RootLayout({
         <Toaster position="top-center" richColors/>
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
